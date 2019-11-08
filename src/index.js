6db793b41e27e0af4c80247255521e770a23d523
Tomas Holderness <tomas@addresscloud.com>
2019-11-08 19:58:15 +0000

    fix workspace var

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ async function main(){
 
         const tf = new Terraform(token, org)
         const id = await tf.run(workspace, filePath)
-        console.log(`Workspace run submitted succesfully: https://app.terraform.io/app/${org}/workspaces/${workspaces}/runs/${id}`)
+        console.log(`Workspace run submitted succesfully: https://app.terraform.io/app/${org}/workspaces/${workspace}/runs/${id}`)
         core.setOutput("runId", id);
     } catch (error) {
         core.setFailed(error.message);

d697d16d91e3f2990529db524d1b3373391596a5
Tomas Holderness <tomas@addresscloud.com>
2019-12-09 13:42:06 +0000

    move identifier slice

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ export default async function main() {
               org = core.getInput('tfOrg'),
               workspace = core.getInput('tfWorkspace'),
               filePath = core.getInput('filePath'),
-              identifier = core.getInput('identifier').slice(0, 7)
+              identifier = core.getInput('identifier')
 
         const tf = new Terraform(token, org)
-        const id = await tf.run(workspace, filePath, identifier)
+        const id = await tf.run(workspace, filePath, identifier.slice(0, 7))
         console.log(`Workspace run submitted succesfully: https://app.terraform.io/app/${org}/workspaces/${workspace}/runs/${id}`)
         core.setOutput("runId", id);
     } catch (error) {

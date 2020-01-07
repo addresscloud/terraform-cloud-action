837faba2e20d3b50412995279c867023e6e60472
Tomas <tomasholderness@gmail.com>
2020-01-07 16:39:21 +0000

    update local rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-    "extends": "@addresscloud"
+    "extends": "@addresscloud",
+    "rules": {
+        "no-await-in-loop": 0,
+        "no-loop-func": 0,
+        "multiline-ternary": 0,
+        "no-ternary": 0,
+        "arrow-body-style": 0,
+        "prefer-destructuring": 0
+    }
 };

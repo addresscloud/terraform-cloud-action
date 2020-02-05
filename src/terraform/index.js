bbfd0b11e9491fc6aa6df835fe31eca32a7d29e5
Tomas Holderness <tomas@addresscloud.com>
2020-02-05 13:33:03 +0000

    remove debugging logs

--- src/terraform/index.js
+++ src/terraform/index.js
@@ -115,10 +115,7 @@ export default class Terraform {
      */
     async _uploadConfiguration(configId, uploadUrl, filePath) {
         try {
-            console.log(filePath)
-            console.log(fs.statSync(filePath).size)
-            const res = await this.axios.put(uploadUrl, fs.createReadStream(filePath), {headers: {'Content-Type': `application/octet-stream`}})
-            console.log(res)
+            await this.axios.put(uploadUrl, fs.createReadStream(filePath), {headers: {'Content-Type': `application/octet-stream`}})
             let status = await this._getConfigVersionStatus(configId),
                 counter = 0
 

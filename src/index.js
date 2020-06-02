96171f13452a97024bce2e8701116cf585500628
Tomas Holderness <tomasholderness@gmail.com>
2020-06-02 21:18:32 +0100

    add new params to action yaml

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ export default async function main() {
               workspace = core.getInput('tfWorkspace'),
               filePath = core.getInput('filePath'),
               identifier = core.getInput('identifier'),
-              awaitApply = core.getInput('awaitApply')
+              awaitApply = core.getInput('awaitApply'),
+              awaitInterval = core.getInput('awaitInterval') * 1000,
+              retryLimit = core.getInput('retryLimit'),
+              debug = core.getInput('debug')
 
-        const tf = new Terraform(token, org)
+        const tf = new Terraform(token, org, 1000, retryLimit, awaitInterval, debug)
         const { runId, status } = await tf.run(workspace, filePath, identifier.slice(0, 7), awaitApply)
         console.log(`Workspace run submitted succesfully: https://app.terraform.io/app/${org}/workspaces/${workspace}/runs/${runId}`)
         console.log(`Run status: ${status}`)
